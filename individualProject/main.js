4eedff41e2e08cefbdb3a59912e75d259e586c11
Lee Panti <86505610+LeeEdbert@users.noreply.github.com>
2022-11-16 08:17:04 -0600

    Delete individualProject directory

--- individualProject/main.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementsByClassName("toggle-btn")[0]
-const navLinks = document.getElementsByClassName("nav-links")[0]
-
-toggleBtn.addEventListener("click", function() {
-    navLinks.classList.toggle("active")
-})
